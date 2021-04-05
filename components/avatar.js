04b76244faf2f8edfe676575b8927db56a34818b
Max Ludvigsson <maxiludvigsson@gmail.com>
2021-04-05 11:31:04 +0800

    Cleanup

--- components/avatar.js
+++
@@ -1,7 +0,0 @@
-export default function Avatar() {
-  return (
-    <div className="flex items-center">
-      <img src="/assets/site/avatar.jpeg" className="w-12 h-12 mr-4 rounded-full" alt="Max Ludvigsson" />
-    </div>
-  )
-}
